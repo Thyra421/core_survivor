3c98e731d7691feafcb576aae7d321ec9b276e1c
Thomas Renon <renonth@gmail.com>
2023-12-11 20:55:59 +0100

    add: create project

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Current { get; private set; }
+
+    private void Awake() {
+        if (Current == null) {
+            Current = this;
+            DontDestroyOnLoad(gameObject);
+        } else
+            Destroy(gameObject);
+    }
+
+    private static IEnumerator LoadSceneAsync(string sceneName) {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Single);
+
+        while (!asyncLoad.isDone)
+            yield return null;
+    }
+
+    public void LoadMenuAsync() {
+        StartCoroutine(LoadSceneAsync("Menu"));
+    }
+
+    public void LoadGameAsync() {
+        StartCoroutine(LoadSceneAsync("Game"));
+    }
+}
