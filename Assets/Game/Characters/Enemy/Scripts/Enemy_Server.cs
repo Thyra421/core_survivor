a1c510a94eeb3830bd96b3e2537b6889bd4c37fb
Thomas Renon <renonth@gmail.com>
2024-01-05 04:20:18 +0100

    wip enemy target priority

--- Assets/Game/Characters/Enemy/Scripts/Enemy_Server.cs
+++ Assets/Game/Characters/Enemy/Scripts/Enemy_Server.cs
@@ -1,10 +1,18 @@
 using JetBrains.Annotations;
 using Mirror;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(EnemyHealth))]
 public partial class Enemy
 {
+    [SerializeField]
+    private EnemyTargetPreference targetPreference;
+
+    [SerializeField]
+    private NavMeshAgent navMeshAgent;
+
     private EnemyHealth _enemyHealth;
     [CanBeNull] public Transform Target { get; private set; }
 
@@ -14,7 +22,7 @@ public partial class Enemy
     private void ServerUpdate()
     {
         if (IsDead) return;
-        
+
         FindBestTarget();
     }
 
@@ -25,33 +33,126 @@ public partial class Enemy
     }
 
     [Server]
-    protected virtual void FindBestTarget()
+    private void FindBestTarget()
     {
-        if (PlayerManager.Current.Players.Count == 0) return;
+        bool hasFoundTarget;
+
+        switch (targetPreference) {
+            case EnemyTargetPreference.core:
+                hasFoundTarget = FindCore() || FindClosest();
+                break;
+            case EnemyTargetPreference.player:
+                hasFoundTarget = FindBestPlayer() || FindClosest();
+                break;
+            case EnemyTargetPreference.environment:
+                hasFoundTarget = FindBestEnvironment() || FindClosest();
+                break;
+            case EnemyTargetPreference.closest:
+                hasFoundTarget = FindClosest();
+                break;
+        }
+    }
+
+    [Server]
+    private bool FindCore()
+    {
+        NavMeshPath path = new();
+
+        if (!navMeshAgent.CalculatePath(EnvironmentManager.Current.Core.position, path))
+            return false;
+
+        Target = EnvironmentManager.Current.Core;
+        return true;
+    }
+
+    [Server]
+    private bool FindBestEnvironment()
+    {
+        if (EnvironmentManager.Current.Environments.Count == 0) return false;
 
         Transform target = null;
         float shortestDistance = float.PositiveInfinity;
 
-        foreach (Player p in PlayerManager.Current.Players)
-        {
-            float distance = Vector3.Distance(transform.position, p.transform.position);
-
-            if (!(distance < shortestDistance)) continue;
-
-            shortestDistance = distance;
-            target = p.transform;
-        }
-        
-        foreach (DestructibleEnvironment e in EnvironmentManager.Current.Environments)
-        {
+        foreach (DestructibleEnvironment e in EnvironmentManager.Current.Environments) {
             float distance = Vector3.Distance(transform.position, e.transform.position);
+            NavMeshPath path = new();
 
-            if (!(distance < shortestDistance)) continue;
+            if (distance > shortestDistance || !navMeshAgent.CalculatePath(e.transform.position, path)) continue;
 
             shortestDistance = distance;
             target = e.transform;
         }
 
+        if (target == null) return false;
+
         Target = target;
+        return true;
     }
+
+    [Server]
+    private bool FindBestPlayer()
+    {
+        if (PlayerManager.Current.Players.Count == 0) return false;
+
+        Transform target = null;
+        float shortestDistance = float.PositiveInfinity;
+
+        foreach (Player p in PlayerManager.Current.Players) {
+            float distance = Vector3.Distance(transform.position, p.transform.position);
+            NavMeshPath path = new();
+
+            if (distance > shortestDistance || !navMeshAgent.CalculatePath(p.transform.position, path)) continue;
+
+            shortestDistance = distance;
+            target = p.transform;
+        }
+
+        if (target == null) return false;
+
+        Target = target;
+        return true;
+    }
+
+    [Server]
+    private bool FindClosest()
+    {
+        if (PlayerManager.Current.Players.Count == 0) return false;
+
+        Transform target = null;
+        float shortestDistance = float.PositiveInfinity;
+
+        foreach (DestructibleEnvironment e in EnvironmentManager.Current.Environments) {
+            float distance = Vector3.Distance(transform.position, e.transform.position);
+            NavMeshPath path = new();
+
+            if (distance > shortestDistance || !navMeshAgent.CalculatePath(e.transform.position, path)) continue;
+
+            shortestDistance = distance;
+            target = e.transform;
+        }
+
+        foreach (Player p in PlayerManager.Current.Players) {
+            float distance = Vector3.Distance(transform.position, p.transform.position);
+            NavMeshPath path = new();
+
+            if (distance > shortestDistance || !navMeshAgent.CalculatePath(p.transform.position, path)) continue;
+
+            shortestDistance = distance;
+            target = p.transform;
+        }
+
+        if (target == null)
+            return false;
+
+        Target = target;
+        return true;
+    }
+}
+
+public enum EnemyTargetPreference
+{
+    closest,
+    player,
+    environment,
+    core
 }
--- Assets/Game/Scripts/Managers/EnvironmentManager.cs
+++ Assets/Game/Scripts/Managers/EnvironmentManager.cs
@@ -1,4 +1,11 @@
+using UnityEngine;
+
 public class EnvironmentManager : Singleton<EnvironmentManager>
 {
     public ListenableList<DestructibleEnvironment> Environments { get; } = new();
+
+    [SerializeField]
+    private Transform core;
+
+    public Transform Core => core;
 }
